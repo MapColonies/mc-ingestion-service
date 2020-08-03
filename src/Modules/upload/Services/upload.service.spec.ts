f3c6a103bb6312d4418724859a1e9fbbf6b8f298
Shahar s <cl_shaharshavit@rnd-hub.com>
2020-08-03 11:39:07 +0300

    fix(tests): fixed tests

--- src/Modules/upload/Services/upload.service.spec.ts
+++ src/Modules/upload/Services/upload.service.spec.ts
@@ -1,13 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { MCLogger } from '@map-colonies/mc-logger';
 import { UploadService } from './Upload.service';
-import { Logger } from '../../logger/Logger';
+import { ImageIndexerHttpClient } from '../../service-clients/ImageIndexer/ImageIndexerHttpClient';
 
 describe('UploadService', () => {
   let service: UploadService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UploadService, { provide: Logger, useValue: {} }],
+      providers: [
+        UploadService,
+        { provide: MCLogger, useValue: {} },
+        { provide: ImageIndexerHttpClient, useValue: {} },
+      ],
     }).compile();
 
     service = module.get<UploadService>(UploadService);
