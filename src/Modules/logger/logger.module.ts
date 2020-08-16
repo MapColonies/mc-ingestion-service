2fba3b221138b494234b1097c6ee0c25b05f685f
Shahar s <65118046+cl-shaharshavit-rnd@users.noreply.github.com>
2020-08-16 17:33:37 +0300

    chore(deps): updated dependencies (#20)

--- src/Modules/logger/logger.module.ts
+++ src/Modules/logger/logger.module.ts
@@ -1,11 +1,11 @@
 import { Module, Global } from '@nestjs/common';
-import { MCLogger } from '@map-colonies/mc-logger';
+import { MCLogger, ILoggerConfig } from '@map-colonies/mc-logger';
 import { readFileSync } from 'fs';
 import * as config from 'config';
 import { join } from 'path';
 
 const getLogger = () => {
-  const loggerConfig = config.get('logger');
+  const loggerConfig: ILoggerConfig = config.get('logger');
   const packagePath = join(__dirname, '../../../package.json');
   const packageData = readFileSync(packagePath, 'utf-8');
   const service = JSON.parse(packageData);
